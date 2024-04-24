5f60027ef971e49cf2944ae45ac75ebef663f6c3
Ayofe Faheez <ayofefaheez@gmail.com>
2024-04-24 17:36:53 +0100

    app functionality updated

--- index.js
+++ index.js
@@ -8,67 +8,82 @@ const addAlarm = document.querySelector('.btn-addAlarm')
 const alarmDetails = document.querySelector('.alarm-details')
 const edit = document.querySelector('.edit')
 const home = document.querySelector('.home')
+const choosenAudioText = document.querySelector('.choosen-audio-text')
 const timeoutBox = document.querySelector('.timeout')
 timeoutBox.innerHTML = ''
 
-addAlarm.addEventListener('click', function () {
-    edit.classList.add('edit-org')
-    home.style.display = 'none'
-    console.log('as')
-})
-
-let historyArr = [];
-let audioArr = []
-let alarmSound = new Audio('audio/mixkit-alarm-tone-996.wav')
-
-upload.addEventListener('change', function () {
-    const file = upload.files[0]
-    alarmSound = new Audio(URL.createObjectURL(file))
-})
-
+let fileChoose = false;
 let timeoutDisplayed = false; 
+let currentAlarmAudioPlaying = null
+// let historyArr = [];
 
+let historyArr = localStorage.getItem('historyData') ? JSON.parse(localStorage.getItem('historyData')) : []
+
+
+let alarmSound = new Audio('audio/mixkit-alarm-tone-996.wav')
+choosenAudioText.textContent = 'Alarm Tone (Default)'
+
+//A function that generate random id
+const generateUniqueID = (idLength) => [...Array(idLength).keys()].map((elem)=>Math.random().toString(36).substr(2, 1)).join("")
+
+
+//Add an Alarm to your alarm list
+addAlarm.addEventListener('click', function () {
+  edit.classList.add('edit-org')
+  home.style.display = 'none'
+  console.log('as')
+})
+
+//When your alarm time -is due, this function shows alarm message
 const timeout = function (label, id, aud) {
+  if (currentAlarmAudioPlaying) {
+    const audio = currentAlarmAudioPlaying;
+    audio.pause()
+  }
+
+  aud.play()
+  aud.loop = true;
+
   let content = `<div class="timeout-overlay">
   <div class="timeout-box">
     <div class="timeout-comp">
-      <h1>${label}</h1>
+      <h1>${label ? label : 'Alarm'}</h1>
       <div class="btn-snooze">Snooze</div>
     </div>
     <div class="btn-stop">Stop</div>
   </div>
 </div>`;
+
 timeoutBox.innerHTML = content;
  document.querySelector('.timeout-box').style.animation = 'slideDown .5s ease-in'
  document.querySelector('.timeout-overlay').style.animation = 'fadeIn .5s ease-in'
+
  const stopBtn = document.querySelector('.btn-stop')
  const snoozeBtn = document.querySelector('.btn-snooze')
+
+ //clears display message
+ function clearMsg () {
+  aud.pause()
+  document.querySelector('.timeout-box').style.animation = 'slideUp .5s ease-out'
+  document.querySelector('.timeout-overlay').style.animation = 'fadeOut .5s ease-in'
+  setTimeout(function () {
+    document.querySelector('.timeout').innerHTML = ''
+  }, 500)
+  currentAlarmAudioPlaying = null
+}
  
+ //Snooze alarm buttton
  snoozeBtn.addEventListener('click', function () {
   snooze(label, id, aud)
-  aud.pause()
-  document.querySelector('.timeout-box').style.animation = 'slideUp .5s ease-out'
-  document.querySelector('.timeout-overlay').style.animation = 'fadeOut .5s ease-in'
-  setTimeout(function () {
-    document.querySelector('.timeout').innerHTML = ''
-  }, 500)
+  clearMsg()
 })
+ 
+//Stop alarm button
  stopBtn.addEventListener('click', function () {
-  aud.pause()
-  document.querySelector('.timeout-box').style.animation = 'slideUp .5s ease-out'
-  document.querySelector('.timeout-overlay').style.animation = 'fadeOut .5s ease-in'
-  setTimeout(function () {
-    document.querySelector('.timeout').innerHTML = ''
-  }, 500)
+  clearMsg()
 })
 
-// settime out
-setTimeout(function () {
-  aud.pause()
-  document.querySelector('.timeout-box').style.animation = 'slideUp .5s ease-out'
-  document.querySelector('.timeout-overlay').style.animation = 'fadeOut .5s ease-in'
-  document.querySelector('.timeout').innerHTML = ''
-}, 4000)
+currentAlarmAudioPlaying = aud
 }
 
 
@@ -79,32 +94,28 @@ function timer(arr) {
       const date = new Date();
       const curHours = date.getHours();
       const curMin = date.getMinutes();
-      const curSec = date.getSeconds();
       const [alarmHour, alarmMin] = ev.time.split(':')  
-      console.log(curHours, curMin, curSec);
-      console.log(alarmHour, alarmMin, ev.id)
       if(Number(curHours) === Number(alarmHour)  && Number(curMin) === Number(alarmMin)) {
           timeout(ev.label, ev.id, ev.audio)
-          ev.audio.play()
-          ev.audio.loop = true;
           clearInterval(setAlarm)
+          console.log(ev)
       }
     }, 1000)
   })
 }
 
-
+//Go back to home page
 const homePage = function () {
   edit.classList.remove('edit-org')
   home.style.display = 'block'
   home.classList.add('home-org')
+  upload.value = '';
 }
 
-
-
+//update alarm lists
 const updateDetails = function (arr) {
   let html = '';
-  arr.forEach((ev, i) => {
+  arr.forEach((ev) => {
     html += `<div class="alarm-detail">
     <div class="">
       <h1 class="alarm-detail-time">${ev.time}</h1>
@@ -120,14 +131,11 @@ const updateDetails = function (arr) {
   homePage()
 }
 
+//Snoozing function 
 function snooze (label, id, aud) {
-  let timeMin = 1;
-let timeSec = 10
-const snoozeHtml = document.querySelectorAll('.snoozed')
-console.log(snoozeHtml)
-snoozeHtml.forEach(ev => {
-    console.log(ev.dataset.snoozeid)
-})
+let timeMin = 2;
+let timeSec = 60;
+
   const snoozeInt = setInterval(function () {
     timeSec--
     const min = String(timeMin - 1).padStart(2, 0)
@@ -139,14 +147,12 @@ snoozeHtml.forEach(ev => {
     }
 
     if (timeMin === 0 && timeSec === 60) {
-      aud.play()
-      timeout(label, id, aud)
+      timeout(arr, label, id, aud)
       clearInterval(snoozeInt)
     }
     snoozeHtml.forEach(ev => {
       if (id === ev.dataset.snoozeid) {
         ev.innerHTML = `Snoozed: ${min}:${sec}`
-        console.log(`Snoozed: ${min}:${sec}`, '⭐⭐⭐💥💥')
       }
     })
   }, 1000)
@@ -156,11 +162,11 @@ snoozeHtml.forEach(ev => {
 setAlarm.addEventListener("click", function (e) {
   e.preventDefault();
   if (alarmTime.value === '') return;
-  const generateUniqueID = (idLength) => [...Array(idLength).keys()].map((elem)=>Math.random().toString(36).substr(2, 1)).join("")
   const id = generateUniqueID(10)
-  let obj = {time: alarmTime.value, label: alarmName.value, id:id, alarm: false, audio: alarmSound}
+  let obj = {time: alarmTime.value, label: alarmName.value, id:id, audio: alarmSound}
   historyArr.push(obj)
-  console.log(historyArr)
+  localStorage.setItem('historyData', JSON.stringify(historyArr))
+  historyArr = JSON.parse(localStorage.getItem('historyData'))
   timer(historyArr);
   updateDetails(historyArr)
   alarmName.value = ''
@@ -169,13 +175,24 @@ setAlarm.addEventListener("click", function (e) {
 
 btnCancel.addEventListener('click', homePage)
 
+upload.addEventListener('change', function () {
+  if (!fileChoose) {
+    const file = upload.files[0]
+    alarmSound = new Audio(URL.createObjectURL(file))
+    choosenAudioText.textContent = '.mp3'
+    fileChoose = true
+  }
+})
 
 let currentlyPlayingAudio = null;
+//updates alarm audio and user can also play audio 
 selectAud.addEventListener('click', function(e) {
     if (e.target.classList.contains('aud')) {
+      if (fileChoose) upload.value = ''
         const aud = e.target;
         console.log(aud.dataset.aud);
         alarmSound = new Audio(`${aud.dataset.aud}`);
+        choosenAudioText.textContent = aud.textContent
     }
 
     if (e.target.classList.contains('play')) { 
@@ -204,3 +221,5 @@ selectAud.addEventListener('click', function(e) {
     }
 });
 
+timer(historyArr)
+updateDetails(historyArr)
